df249d80196245768b5595326d61725a73f487c5
Guilherme Gelmi <ggelmisalvo@gmail.com>
2025-03-14 20:23:13 +0100

    chore: lastest results

--- projet/src/graph_it.py
+++ projet/src/graph_it.py
@@ -9,8 +9,8 @@ def ler_valores(arquivo):
 
 
 # Ler os valores dos dois arquivos
-valores_arquivo1 = ler_valores('update-time-troisieme.txt')
-valores_arquivo2 = ler_valores('display-time-troisieme.txt')  # Substitua pelo nome do segundo arquivo
+valores_arquivo1 = ler_valores('update-time-1.txt')
+valores_arquivo2 = ler_valores('display-time-1.txt')  # Substitua pelo nome do segundo arquivo
 
 # Gerar as iterações (índices)
 iteracoes = range(len(valores_arquivo1))
@@ -21,12 +21,12 @@ plt.plot(iteracoes, valores_arquivo1, label='update time')
 plt.plot(iteracoes2, valores_arquivo2, label='display time')
 
 # Adicionar título e labels
-plt.title('Valores em função da iteração')
-plt.xlabel('Iteração')
-plt.ylabel('Valores')
-
+plt.title('Loop time per iteration')
+plt.xlabel('Iteration')
+plt.ylabel('Time (s)')
 # Adicionar legenda
 plt.legend()
 
 # Mostrar o gráfico
+plt.savefig('time_analysis.png', dpi=300)
 plt.show()
--- projet/src/model.cpp
+++ projet/src/model.cpp
@@ -80,96 +80,83 @@ Model::update(float * time_update, float * time_for)
 {
     double start = omp_get_wtime();
     std::vector<std::uint8_t> next_front = m_fire_front;
-    std::vector<std::size_t> global_ignite;
+    std::vector<std::size_t> local_ignite;
+    for (std::size_t i = 0; i < m_fire_front.size(); ++i) {
+        if (m_fire_front[i] == 0) continue;
 
-    #pragma omp parallel
-    {
-        std::vector<std::size_t> local_ignite;
+        LexicoIndices coord = get_lexicographic_from_index(i);
+        double power = log_factor(m_fire_front[i]);
 
-        #pragma omp for
-        for (std::size_t i = 0; i < m_fire_front.size(); ++i) {
-            if (m_fire_front[i] == 0) continue;
-
-            LexicoIndices coord = get_lexicographic_from_index(i);
-            double power = log_factor(m_fire_front[i]);
-
-            // Verificação dos vizinhos e coleta em local_ignite
-            if (coord.row < m_geometry-1)
+        // Verificação dos vizinhos e coleta em local_ignite
+        if (coord.row < m_geometry-1)
+        {
+            double tirage      = pseudo_random( i + m_time_step, m_time_step );
+            double green_power = m_vegetation_map[i + m_geometry];
+            double correction  = power * log_factor(green_power);
+            if (tirage < alphaSouthNorth * p1 * correction)
             {
-                double tirage      = pseudo_random( i + m_time_step, m_time_step );
-                double green_power = m_vegetation_map[i + m_geometry];
-                double correction  = power * log_factor(green_power);
-                if (tirage < alphaSouthNorth * p1 * correction)
-                {
-                    local_ignite.push_back(i + m_geometry);
-                }
-            }
-
-            if (coord.row > 0)
-            {
-                double tirage      = pseudo_random( i * 13427 + m_time_step, m_time_step );
-                double green_power = m_vegetation_map[i - m_geometry];
-                double correction  = power * log_factor(green_power);
-                if (tirage < alphaNorthSouth * p1 * correction)
-                {
-                    local_ignite.push_back(i - m_geometry);
-                }
-            }
-
-            if (coord.column < m_geometry-1)
-            {
-                double tirage      = pseudo_random( i * 13427 * 13427 + m_time_step, m_time_step );
-                double green_power = m_vegetation_map[i + 1];
-                double correction  = power * log_factor(green_power);
-                if (tirage < alphaEastWest * p1 * correction)
-                {
-                    local_ignite.push_back(i + 1);
-                }
-            }
-
-            if (coord.column > 0)
-            {
-                double tirage      = pseudo_random( i * 13427 * 13427 * 13427 + m_time_step, m_time_step );
-                double green_power = m_vegetation_map[i - 1];
-                double correction  = power * log_factor(green_power);
-                if (tirage < alphaWestEast * p1 * correction)
-                {
-                    local_ignite.push_back(i - 1);
-                }
-            }
-
-            // Processamento seguro do estado atual da célula
-            if (m_fire_front[i] == 255)
-            {
-                double tirage = pseudo_random( i * 52513 + m_time_step, m_time_step );
-                if (tirage < p2)
-                {
-                    m_fire_map[i] >>= 1;
-                    next_front[i] >>= 1;
-                }
-            }
-            else
-            {
-                m_fire_map[i] >>= 1;
-                next_front[i] >>= 1; 
+                local_ignite.push_back(i + m_geometry);
             }
         }
 
-        // Combinação segura das listas locais
-        #pragma omp critical
-        global_ignite.insert(global_ignite.end(), local_ignite.begin(), local_ignite.end());
+        if (coord.row > 0)
+        {
+            double tirage      = pseudo_random( i * 13427 + m_time_step, m_time_step );
+            double green_power = m_vegetation_map[i - m_geometry];
+            double correction  = power * log_factor(green_power);
+            if (tirage < alphaNorthSouth * p1 * correction)
+            {
+                local_ignite.push_back(i - m_geometry);
+            }
+        }
+
+        if (coord.column < m_geometry-1)
+        {
+            double tirage      = pseudo_random( i * 13427 * 13427 + m_time_step, m_time_step );
+            double green_power = m_vegetation_map[i + 1];
+            double correction  = power * log_factor(green_power);
+            if (tirage < alphaEastWest * p1 * correction)
+            {
+                local_ignite.push_back(i + 1);
+            }
+        }
+
+        if (coord.column > 0)
+        {
+            double tirage      = pseudo_random( i * 13427 * 13427 * 13427 + m_time_step, m_time_step );
+            double green_power = m_vegetation_map[i - 1];
+            double correction  = power * log_factor(green_power);
+            if (tirage < alphaWestEast * p1 * correction)
+            {
+                local_ignite.push_back(i - 1);
+            }
+        }
+
+        // Processamento seguro do estado atual da célula
+        if (m_fire_front[i] == 255)
+        {
+            double tirage = pseudo_random( i * 52513 + m_time_step, m_time_step );
+            if (tirage < p2)
+            {
+                m_fire_map[i] >>= 1;
+                next_front[i] >>= 1;
+            }
+        }
+        else
+        {
+            m_fire_map[i] >>= 1;
+            next_front[i] >>= 1; 
+        }
     }
-    //*time_update += (omp_get_wtime() - start);
+
     double for_start = omp_get_wtime();
-    #pragma omp parallel for
-    for (std::size_t i = 0; i < global_ignite.size(); ++i) {
-        m_fire_map[global_ignite[i]] = 255;
-        next_front[global_ignite[i]] = 255;
+    for (std::size_t i = 0; i < local_ignite.size(); ++i) {
+        m_fire_map[local_ignite[i]] = 255;
+        next_front[local_ignite[i]] = 255;
     }
     m_keys_by_step.push_back(m_fire_front);
     m_fire_front.swap(next_front);
     bool empty = true;
-    #pragma omp parallel for
     for (std::size_t i = 0; i < m_fire_front.size(); ++i) {
         if (m_fire_front[i] > 0) { 
             empty = false;
